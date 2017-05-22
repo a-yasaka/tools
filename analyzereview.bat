f7b0a31cd82a5c9ee27201d5fb41882a505367ab
lets note home <a-yasaka@nri.co.jp>
2017-05-22 20:42:46 +0900

    ディレクトリ修正

--- analyzereview.bat
+++ analyzereview.bat
@@ -1,15 +1,15 @@
-del c:\users\nriuser\desktopcranalyzereviews_gai2.csv
-del c:\users\nriuser\desktopcranalyzemeetings_gai2.csv
-del c:\users\nriuser\desktopcranalyzedocuments_gai2.csv
-del c:\users\nriuser\desktopcranalyzereviews_nai2.csv
-del c:\users\nriuser\desktopcranalyzemeetings_nai2.csv
-del c:\users\nriuser\desktopcranalyzedocuments_nai2.csv
-del c:\users\nriuser\desktopcranalyzereviews_det2.csv
-del c:\users\nriuser\desktopcranalyzemeetings_det2.csv
-del c:\users\nriuser\desktopcranalyzedocuments_det2.csv
-del c:\users\nriuser\desktopcranalyzereviews_make2.csv
-del c:\users\nriuser\desktopcranalyzereviews_ut2.csv
-del c:\users\nriuser\desktopcranalyzebugs_ut2.csv
+del c:\users\nriuser\desktop\cranalyzereviews_gai2.csv
+del c:\users\nriuser\desktop\cranalyzemeetings_gai2.csv
+del c:\users\nriuser\desktop\cranalyzedocuments_gai2.csv
+del c:\users\nriuser\desktop\cranalyzereviews_nai2.csv
+del c:\users\nriuser\desktop\cranalyzemeetings_nai2.csv
+del c:\users\nriuser\desktop\cranalyzedocuments_nai2.csv
+del c:\users\nriuser\desktop\cranalyzereviews_det2.csv
+del c:\users\nriuser\desktop\cranalyzemeetings_det2.csv
+del c:\users\nriuser\desktop\cranalyzedocuments_det2.csv
+del c:\users\nriuser\desktop\cranalyzereviews_make2.csv
+del c:\users\nriuser\desktop\cranalyzereviews_ut2.csv
+del c:\users\nriuser\desktop\cranalyzebugs_ut2.csv
 
 jruby c:\users\nriuser\tools\makeimportfile.rb
 psql --dbname=local --username=postgres -f c:/users/nriuser/tools/setup.sql
