aa348ab51b4c055460a213fbe8256a5c56363ced
A. Yasaka <a-yasaka@nri.co.jp>
2017-06-22 10:20:26 +0900

    パス誤り修正

--- analyzereview.bat
+++ analyzereview.bat
@@ -1,15 +1,15 @@
-del c:\users\nriuser\desktop\cranalyzereviews_gai2.csv
-del c:\users\nriuser\desktop\cranalyzemeetings_gai2.csv
-del c:\users\nriuser\desktop\cranalyzedocuments_gai2.csv
-del c:\users\nriuser\desktop\cranalyzereviews_nai2.csv
-del c:\users\nriuser\desktop\cranalyzemeetings_nai2.csv
-del c:\users\nriuser\desktop\cranalyzedocuments_nai2.csv
-del c:\users\nriuser\desktop\cranalyzereviews_det2.csv
-del c:\users\nriuser\desktop\cranalyzemeetings_det2.csv
-del c:\users\nriuser\desktop\cranalyzedocuments_det2.csv
-del c:\users\nriuser\desktop\cranalyzereviews_make2.csv
-del c:\users\nriuser\desktop\cranalyzereviews_ut2.csv
-del c:\users\nriuser\desktop\cranalyzebugs_ut2.csv
+del c:\users\nriuser\desktop\cranalyze\reviews_gai2.csv
+del c:\users\nriuser\desktop\cranalyze\meetings_gai2.csv
+del c:\users\nriuser\desktop\cranalyze\documents_gai2.csv
+del c:\users\nriuser\desktop\cranalyze\reviews_nai2.csv
+del c:\users\nriuser\desktop\cranalyze\meetings_nai2.csv
+del c:\users\nriuser\desktop\cranalyze\documents_nai2.csv
+del c:\users\nriuser\desktop\cranalyze\reviews_det2.csv
+del c:\users\nriuser\desktop\cranalyze\meetings_det2.csv
+del c:\users\nriuser\desktop\cranalyze\documents_det2.csv
+del c:\users\nriuser\desktop\cranalyze\reviews_make2.csv
+del c:\users\nriuser\desktop\cranalyze\reviews_ut2.csv
+del c:\users\nriuser\desktop\cranalyze\bugs_ut2.csv
 
 jruby c:\users\nriuser\tools\makeimportfile.rb
 psql --dbname=local --username=postgres -f c:/users/nriuser/tools/setup.sql
