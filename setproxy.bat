6bdf1c737d29c7d2a67a1f0eb37ae63729572f0f
A. Yasaka <a-yasaka@nri.co.jp>
2018-02-19 11:54:57 +0900

    proxy tool added

---
+++ setproxy.bat
@@ -0,0 +1,10 @@
+@echo off
+IF "%1"=="" (
+	echo 引数にPROXYのパスワードを設定してください
+) ELSE (
+	set HTTP_PROXY_PREFIX=http://m3975:
+	set HTTPS_PROXY_PREFIX=https://m3975:
+	set PROXY_ADR=@nriproxy.nri.co.jp:86
+	set HTTP_PROXY=%HTTP_PROXY_PREFIX%%1%PROXY_ADR%
+	set HTTPS_PROXY=%HTTPS_PROXY_PREFIX%%1%PROXY_ADR%
+)
