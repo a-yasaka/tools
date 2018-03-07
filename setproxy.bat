fc6e390b9f864d57e64d73c64591525204b4f378
A. Yasaka <a-yasaka@nri.co.jp>
2018-03-07 10:29:30 +0900

    setproxy changed

--- setproxy.bat
+++ setproxy.bat
@@ -2,9 +2,6 @@
 IF "%1"=="" (
 	echo 引数にPROXYのパスワードを設定してください
 ) ELSE (
-	set HTTP_PROXY_PREFIX=http://m3975:
-	set HTTPS_PROXY_PREFIX=https://m3975:
-	set PROXY_ADR=@nriproxy.nri.co.jp:86
-	setx HTTP_PROXY %HTTP_PROXY_PREFIX%%1%PROXY_ADR%
-	setx HTTPS_PROXY %HTTPS_PROXY_PREFIX%%1%PROXY_ADR%
+	set HTTP_PROXY=http://m3975:%1@nriproxy.nri.co.jp:86
+	set HTTPS_PROXY=https://m3975:%1@nriproxy.nri.co.jp:86
 )
